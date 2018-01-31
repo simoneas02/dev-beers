067a71c559923b4900faa7ec1439f614dcea15be
simoneas02 <simoneas03@gmail.com>
2018-01-31 18:44:34 -0300

    start inictial project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Dev Beers</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
