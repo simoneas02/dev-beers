088f95cc6891a9e91f594f761b551d658e4f76f8
simoneas02 <simoneas03@gmail.com>
2018-02-02 17:39:32 -0300

    add style for header and update style for footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,15 @@ import beer from '../assets/img/beer.png';
 
 const Header = () => (
   <header className='header'>
-    <GithubCorner size={80} bannerColor='$secondary-color' href='https://github.com/simoneas02/beta-contrata#readme' />
+    <GithubCorner size={80} octoColor="#363636" bannerColor='#fffc00' href='https://github.com/simoneas02/dev-beers#readme' />
     
     <div className='header__block'>
       <h1 className='header__block__title'>Dev Beers</h1>
-      <p className='header__block__description'>Beers list with React/Redux</p>
-      <img className='header__block__img' src={beer} alt='beer' />
+
+      <div className='header__block__text'>
+        <p className='header__block__description'>Beers list with React / Redux</p>
+        <img className='header__block__img' src={beer} alt='beer' />
+      </div>
     </div>
   </header>
 );
